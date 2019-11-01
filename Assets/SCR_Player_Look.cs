84ca5c9d664f153e7bfd385c499bd32526f7e3a6
Tomas Rutkauskas <36405450+ChaoticCookie@users.noreply.github.com>
2019-11-01 11:09:15 +0000

    FPS Basic Mechanics
    Move
    Camera
    Jump

---
+++ Assets/SCR_Player_Look.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_Player_Look : MonoBehaviour
+{
+
+    [SerializeField] private string mouseXInputName, mouseYInputName;
+    [SerializeField] private float mouseSensitivity;
+
+    [SerializeField] private Transform PlayerBody;
+    private float xAxisClap;
+
+
+    private void Awake()
+    {
+        LockCursor();
+        xAxisClap = 0.0f;
+    }
+
+
+    private void LockCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        CameraRotation();
+    }
+
+    private void CameraRotation()
+    {
+        float mouseX = Input.GetAxis(mouseXInputName) * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis(mouseYInputName) * mouseSensitivity * Time.deltaTime;
+
+        xAxisClap += mouseY;
+
+        if(xAxisClap > 90.0f)
+        {
+            xAxisClap = 90.0f;
+            mouseY = 0.0f;
+            clampXAxisRotationSetValue(270.0f);
+        }
+        else if (xAxisClap < -90.0f)        
+        {
+            xAxisClap = -90.0f;
+            mouseY = 0.0f;
+            clampXAxisRotationSetValue(90.0f);
+        }
+
+        transform.Rotate(Vector3.left * mouseY);        //transform.Rotate(-transform.right * mouseY);
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    private void clampXAxisRotationSetValue(float value)
+    {
+        Vector3 eulerRotation = transform.eulerAngles;
+        eulerRotation.x = value;
+        transform.eulerAngles = eulerRotation;
+    }
+}
